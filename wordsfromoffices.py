fcb29482c4414fbd39c563b0feab889947b5b619
Michael Lambright <michaelglambright@gmail.com>
2014-04-04 12:22:21 -0400

    fixed double words

--- wordsfromoffices.py
+++ wordsfromoffices.py
@@ -4,7 +4,7 @@ def wordsfromoffices(limit):
   db = MySQLdb.connect(host='173.255.254.42', db='fellowbot',read_default_file='~/.my.cnf')
   c = db.cursor()
   if len(limit) > 0:
-    query = 'select uid, office_name, official_name, official_party from offices where {0};'.format(limit)
+    query = 'select uid, office_name, official_name, official_party from offices {0};'.format(limit)
   else:
     query = 'select uid, office_name, official_name, official_party from offices;'
   c.execute(query)
@@ -17,13 +17,17 @@ def wordsfromoffices(limit):
   for item in officeinfo:
     print item
     for i in range(3):
-      words = item[i+1].lower().replace(",","").replace("'","\\'").split(" ")
+      words = item[i+1].lower().replace(","," ").replace("'","\\'").replace("-"," ").replace("   "," ").replace("  "," ").split(" ")
       for element in words:
         idquery = "select idwords from words where word = '{0}';".format(element)
         insertword = "insert into words (word) values ('{0}');".format(element)
         result = c.execute(idquery)
         if result > 0:
           wordid = c.fetchone()[0]
+          checkexists = "select * from offices_has_words where offices_uid = '{0}' and words_idwords ='{1}';".format(item[0],wordid)
+          test = c.execute(checkexists)
+          if test > 0:
+            continue
         else:
           c.execute(insertword)
           db.commit()
@@ -34,4 +38,4 @@ def wordsfromoffices(limit):
         db.commit()
   db.close()
 
-wordsfromoffices('')
+wordsfromoffices('left outer join offices_has_words on uid=offices_uid where offices_uid is NULL')
