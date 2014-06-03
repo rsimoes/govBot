82bb5659e0b3d5367896e917a9e313e3086c8a1d
Michael Lambright <michaelglambright@gmail.com>
2014-06-03 11:27:49 -0400

    Add AL

---
+++ attended/SL/ALLeg.py
@@ -0,0 +1,93 @@
+import urllib2, re
+from bs4 import BeautifulSoup
+from csv import DictWriter
+
+def getALrep(url):
+  print url
+  email = ''
+  response = urllib2.urlopen(url)
+
+  if response.code == 200:
+    soup = BeautifulSoup(response.read())
+
+    tempEmail = soup.find('a', {'href': re.compile('mailto')})
+    if tempEmail is not None:
+      email = re.sub('[Mm][Aa][Ii][Ll][Tt][Oo]:', '', tempEmail.get('href'))
+
+  return email
+
+
+def getALLeg(partyDict):
+  houseSoup = BeautifulSoup(urllib2.urlopen('http://www.legislature.state.al.us/house/representatives/houseroster_alpha.html').read())
+  senateSoup = BeautifulSoup(urllib2.urlopen('http://www.legislature.state.al.us/senate/senators/senateroster_alpha.html').read())
+
+  houseTable = houseSoup.find('table', {'height': '3360'}).find_all('tr')
+  senateTable = senateSoup.find('table', {'height': '888'}).find_all('tr')
+
+  dictList = []
+
+  for i in range(1, len(houseTable)):
+    repInfo = {}
+    columns = houseTable[i].find_all('td')
+
+    link = columns[0].find('a')
+
+    if link.get('href') != '#':
+      repInfo['Website'] = 'http://www.legislature.state.al.us/house/representatives/' + link.get('href')
+      repInfo['Email'] = getALrep(repInfo['Website'])
+
+    rawName = link.get_text().split(',')
+
+    if len(rawName) == 2:
+      repInfo['Name'] = rawName[1].strip().title() + ' ' + rawName[0].strip().title()
+    elif len(rawName) == 3:
+      repInfo['Name'] = rawName[1].strip().title() + ' ' + rawName[0].strip().title() + ' ' + rawName[2].strip().title()
+    else:
+      repInfo['Name'] = link.string.strip().title()
+
+    repInfo['Party'] = partyDict[str(columns[1].get_text().strip())]
+    repInfo['District'] = 'AL State House District ' + columns[2].get_text().strip()
+    repInfo['Address'] = 'Room ' + columns[3].get_text().strip() + ' 11 South Union Street Montomery, AL 36130'
+    repInfo['Phone'] = columns[4].get_text().strip()
+
+    dictList.append(repInfo)
+
+  for i in range(1, len(senateTable)):
+    repInfo = {}
+    columns = senateTable[i].find_all('td')
+
+    link = columns[0].find('a')
+
+    if link.get('href') != '#':
+      repInfo['Website'] = 'http://www.legislature.state.al.us/senate/senators/' + link.get('href')
+      repInfo['Email'] = getALrep(repInfo['Website'])
+
+    rawName = link.get_text().split(',')
+
+    if len(rawName) == 2:
+      repInfo['Name'] = rawName[1].strip().title() + ' ' + rawName[0].strip().title()
+    elif len(rawName) == 3:
+      repInfo['Name'] = rawName[1].strip().title() + ' ' + rawName[0].strip().title() + ' ' + rawName[2].strip().title()
+    else:
+      repInfo['Name'] = link.string.strip().title()
+
+    repInfo['Party'] = partyDict[str(columns[1].get_text().strip())]
+    repInfo['District'] = 'AL State Senate District ' + columns[2].get_text().strip()
+    repInfo['Address'] = 'Room ' + columns[3].get_text().strip() + ' 11 South Union Street Montomery, AL 36130'
+    repInfo['Phone'] = columns[4].get_text().strip()
+
+    dictList.append(repInfo)
+
+  return dictList
+
+if __name__ == '__main__':
+  partyDict = {'(R)': 'Republican', '(D)': 'Democratic', '(I)':'Independent', 'R': 'Republican', 'D': 'Democratic', '': 'Unknown', 'I': 'Independent', 'Democrat': 'Democratic', 'Republican': 'Republican', 'Democratic': 'Democratic', 'Independent': 'Independent'}
+  dictList = getALLeg(partyDict)
+
+  with open('/home/michael/Desktop/ALLeg.csv', 'w') as csvFile:
+    dwObject = DictWriter(csvFile, ['District', 'Name', 'Party', 'Website', 'Email', 'Phone', 'Address'], restval='')
+    dwObject.writeheader()
+    
+    for row in dictList:
+      dwObject.writerow(row)
+
