c730d99c458b9e9123a8a26d166cdc8f0411b247
Michael Lambright <michaelglambright@gmail.com>
2014-06-11 17:53:21 -0400

    removed print

--- attended/SL/NHLeg.py
+++ attended/SL/NHLeg.py
@@ -14,7 +14,7 @@ def getNHLeg(partyDict):
 
   for item in houseTable:
     repInfo = {}
-    print item
+
     repInfo['District'] = 'NH State {0} District {1} {2}'.format(item['chamber'], item['town'], item['district'])
     repInfo['Party'] = partyDict[str(item['party'])]
     repInfo['Name'] = '{0} {1} {2}'.format(item['firstName'], item['middleName'], item['lastName']).strip().replace('    ', ' ').replace('  ', ' ').replace(u'\u00A0', ' ').replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
