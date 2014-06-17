b1a4021e459a8abe49882c056d861bf82b9a6ab5
Michael Lambright <michaelglambright@gmail.com>
2014-06-17 12:03:46 -0400

    actually WI Senate now

--- attended/SL/WILeg.py
+++ attended/SL/WILeg.py
@@ -5,10 +5,10 @@ from csv import DictWriter
 
 def getWILeg(partyDict):
   houseSoup = BeautifulSoup(urllib2.urlopen('http://legis.wisconsin.gov/Pages/leg-list.aspx?h=a').read(), 'lxml')
-  senateSoup = BeautifulSoup(urllib2.urlopen('http://www.legis.state.wv.us/Senate1/roster.cfm').read(), 'lxml')
+  senateSoup = BeautifulSoup(urllib2.urlopen('http://legis.wisconsin.gov/Pages/leg-list.aspx?h=s').read(), 'lxml')
   
   houseTable = houseSoup.find('table', {'class': 'legis-list'}).find_all('tr')
-  senateTable = senateSoup.find('table', {'class': 'tabborder'}).find_all('tr', {'valign': 'top'})
+  senateTable = senateSoup.find('table', {'class': 'legis-list'}).find_all('tr')
 
   dictList = []
 
@@ -37,20 +37,30 @@ def getWILeg(partyDict):
 
       dictList.append(repInfo)
 
-  for item in senateTable:
+  for i in range(1, len(senateTable)):
     repInfo = {}
-    columns = item.find_all('td')
-    link = columns[0].find('a', {'style': 'vertical-align:top'})
+    columns = senateTable[i].find_all('td')
 
-    repInfo['Name'] = link.get_text().replace(u'\u00A0', ' ').strip().replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
-    repInfo['Website'] = 'http://www.legis.state.wv.us/Senate1/' + link.get('href')
-    repInfo['Party'] = partyDict[str(columns[1].get_text().strip())]
-    repInfo['District'] = 'WI State Senate District {0}'.format(int(columns[2].get_text().strip()))
-    repInfo['Email'] = columns[4].get_text().strip()
-    repInfo['Phone'] = columns[5].get_text().strip()
-    repInfo['Address'] = columns[3].get_text().strip()
+    if len(columns) > 1:
+      link = columns[0].find('a')
 
-    dictList.append(repInfo)
+      identityText = link.get_text().replace('\n', ' ').strip()
+      nameText = re.sub(r'^(.*)\(.*$', r'\1', identityText)
+
+      nameList = nameText.split(',')
+      if len(nameList) == 2:
+        name = nameList[1].strip() + ' ' + nameList[0].strip()
+      elif len(nameList) == 3:
+        name = nameList[1].strip() + ' ' + nameList[2].strip() + ' ' + nameList[0].strip()
+      else:
+        name = nameText.strip()
+
+      repInfo['Name'] = name.replace(u'\u00A0', ' ').strip().replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
+      repInfo['Website'] = 'http://legis.wisconsin.gov/Pages/' + link.get('href')
+      repInfo['Party'] = partyDict[str(re.sub(r'^.*\((.)\).*$', r'\1', identityText))]
+      repInfo['District'] = 'WI State Senate District {0}'.format(columns[2].get_text().strip())
+
+      dictList.append(repInfo)
 
   return dictList
 
