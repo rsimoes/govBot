cacdbffb2ba74ede619bc99c16b2aeb59dc14a9e
Michael Lambright <michaelglambright@gmail.com>
2014-06-25 15:59:51 -0400

    Account for resignations in AZ

--- attended/SL/AZLeg.py
+++ attended/SL/AZLeg.py
@@ -33,15 +33,16 @@ def getAZLeg(partyDict):
     columns = item.find_all('td')
     link = columns[0].find('a')
 
-    repInfo['Name'] = link.get_text().strip().replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
-    repInfo['Website'] = 'http://www.azleg.gov' + link.get('href')
-    repInfo['District'] = 'AZ State House District {0}'.format(columns[1].get_text().strip())
-    repInfo['Party'] = partyDict[str(columns[2].get_text().strip())]
-    repInfo['Email'] = re.sub('[Mm][Aa][Ii][Ll][Tt][Oo]:', '', columns[3].find('a').get('href'))
-    repInfo['Address'] = '1700 West Washington, Room {0}, Phoenix, AZ 85007-2890'.format(columns[4].get_text().strip())
-    repInfo['Phone'] = '(602)-{0}'.format(columns[5].get_text().strip())
+    if link.get('class') is None:
+      repInfo['Name'] = link.get_text().strip().replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
+      repInfo['Website'] = 'http://www.azleg.gov' + link.get('href')
+      repInfo['District'] = 'AZ State House District {0}'.format(columns[1].get_text().strip())
+      repInfo['Party'] = partyDict[str(columns[2].get_text().strip())]
+      repInfo['Email'] = re.sub('[Mm][Aa][Ii][Ll][Tt][Oo]:', '', columns[3].find('a').get('href'))
+      repInfo['Address'] = '1700 West Washington, Room {0}, Phoenix, AZ 85007-2890'.format(columns[4].get_text().strip())
+      repInfo['Phone'] = '(602)-{0}'.format(columns[5].get_text().strip())
 
-    dictList.append(repInfo)
+      dictList.append(repInfo)
 
   for i in range(1, len(senateTable)):
     item = senateTable[i]
@@ -50,15 +51,16 @@ def getAZLeg(partyDict):
     columns = item.find_all('td')
     link = columns[0].find('a')
 
-    repInfo['Name'] = link.get_text().strip().replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
-    repInfo['Website'] = 'http://www.azleg.gov' + link.get('href')
-    repInfo['District'] = 'AZ State Senate District {0}'.format(columns[1].get_text().strip())
-    repInfo['Party'] = partyDict[str(columns[2].get_text().strip())]
-    repInfo['Email'] = re.sub('[Mm][Aa][Ii][Ll][Tt][Oo]:', '', columns[3].find('a').get('href'))
-    repInfo['Address'] = '1700 West Washington, Room {0}, Phoenix, AZ 85007-2890'.format(columns[4].get_text().strip())
-    repInfo['Phone'] = '(602)-{0}'.format(columns[5].get_text().strip())
+    if link.get('class') is None:
+      repInfo['Name'] = link.get_text().strip().replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
+      repInfo['Website'] = 'http://www.azleg.gov' + link.get('href')
+      repInfo['District'] = 'AZ State Senate District {0}'.format(columns[1].get_text().strip())
+      repInfo['Party'] = partyDict[str(columns[2].get_text().strip())]
+      repInfo['Email'] = re.sub('[Mm][Aa][Ii][Ll][Tt][Oo]:', '', columns[3].find('a').get('href'))
+      repInfo['Address'] = '1700 West Washington, Room {0}, Phoenix, AZ 85007-2890'.format(columns[4].get_text().strip())
+      repInfo['Phone'] = '(602)-{0}'.format(columns[5].get_text().strip())
 
-    dictList.append(repInfo)
+      dictList.append(repInfo)
 
   return dictList
 
