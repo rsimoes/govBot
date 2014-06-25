03b2d154b6d4423396a133e01cb1a1b98082c7a3
Michael Lambright <michaelglambright@gmail.com>
2014-06-25 16:32:10 -0400

    Fix assembly districts

--- attended/SL/CALeg.py
+++ attended/SL/CALeg.py
@@ -29,7 +29,7 @@ def getCALeg(partyDict):
         repInfo['Name'] = link.get_text().strip().replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
 
       repInfo['Website'] = link.get('href')
-    repInfo['District'] = 'CA State Assembly District {0}'.format(item.find('td', {'class': 'views-field views-field-field-member-district-value'}).get_text().strip())
+    repInfo['District'] = 'CA State Assembly District {0}'.format(int(item.find('td', {'class': 'views-field views-field-field-member-district-value'}).get_text().strip()))
     repInfo['Party'] = partyDict[str(item.find('td', {'class': 'views-field views-field-field-member-party-value'}).get_text().strip())]
 
     rawContact = item.find('td', {'class': 'views-field views-field-field-member-feedbackurl-value'}).find('p').get_text().split(';')
