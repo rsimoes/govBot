5fc3b664be0e3c6f1d879699f1895855bc3aeded
Michael Lambright <michaelglambright@gmail.com>
2014-07-21 14:09:13 -0400

    account for NC notices

--- attended/SL/NCLeg.py
+++ attended/SL/NCLeg.py
@@ -36,32 +36,42 @@ def getNCLeg(partyDict):
     columns = item.find_all('td')\
 
     if len(columns) != 0:
-      link = columns[2].find('a')
+      skip = False
+      if columns[2].find('span', {'class': 'notice'}) is not None:
+        if re.search(r'(Resigned)|(Deceased)', columns[2].find('span', {'class': 'notice'}).get_text().strip()):
+          skip = True
+      if not skip:
+        link = columns[2].find('a')
 
-      repInfo['Name'] = link.get_text().strip().replace(u'\u00A0', ' ').replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
-      repInfo['Website'] = 'http://www.ncleg.net' + link.get('href')
-      repInfo['District'] = 'NC State House District {0}'.format(columns[1].get_text().strip())
-      repInfo['Party'] = partyDict[str(columns[0].get_text().strip())]
+        repInfo['Name'] = link.get_text().strip().replace(u'\u00A0', ' ').replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
+        repInfo['Website'] = 'http://www.ncleg.net' + link.get('href')
+        repInfo['District'] = 'NC State House District {0}'.format(columns[1].get_text().strip())
+        repInfo['Party'] = partyDict[str(columns[0].get_text().strip())]
 
-      repInfo['Email'] = getNCRep(repInfo['Website'])
+        repInfo['Email'] = getNCRep(repInfo['Website'])
 
-      dictList.append(repInfo)
+        dictList.append(repInfo)
   
   for item in senateTable:
     repInfo = {}
     columns = item.find_all('td')
 
     if len(columns) != 0:
-      link = columns[2].find('a')
+      skip = False
+      if columns[2].find('span', {'class': 'notice'}) is not None:
+        if re.search(r'(Resigned)|(Deceased)', columns[2].find('span', {'class': 'notice'}).get_text().strip()):
+          skip = True
+      if not skip:
+        link = columns[2].find('a')
 
-      repInfo['Name'] = link.get_text().strip().replace(u'\u00A0', ' ').replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
-      repInfo['Website'] = 'http://www.ncleg.net' + link.get('href')
-      repInfo['District'] = 'NC State Senate District {0}'.format(columns[1].get_text().strip())
-      repInfo['Party'] = partyDict[str(columns[0].get_text().strip())]
+        repInfo['Name'] = link.get_text().strip().replace(u'\u00A0', ' ').replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
+        repInfo['Website'] = 'http://www.ncleg.net' + link.get('href')
+        repInfo['District'] = 'NC State Senate District {0}'.format(columns[1].get_text().strip())
+        repInfo['Party'] = partyDict[str(columns[0].get_text().strip())]
 
-      repInfo['Email'] = getNCRep(repInfo['Website'])
+        repInfo['Email'] = getNCRep(repInfo['Website'])
 
-      dictList.append(repInfo)
+        dictList.append(repInfo)
 
   return dictList
   
