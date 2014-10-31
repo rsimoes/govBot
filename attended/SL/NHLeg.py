0f10d8d1e3dab14ab4a4483aebe4d46a9d198171
Michael Lambright <michael.g.lambright@gmail.com>
2014-10-31 11:47:49 -0400

    fix district numbers

--- attended/SL/NHLeg.py
+++ attended/SL/NHLeg.py
@@ -7,7 +7,7 @@ def getNHLeg(partyDict):
   houseText = urllib2.urlopen('http://www.gencourt.state.nh.us/downloads/Members.txt').read()
   senateSoup = BeautifulSoup(urllib2.urlopen('http://www.gencourt.state.nh.us/senate/members/senate_roster.aspx').read(), 'lxml')
 
-  houseTable = DictReader(StringIO(houseText), delimiter = '\t', fieldnames = ['chamber', 'fullName', 'lastName', 'firstName', 'middleName', 'town', 'district', 'id', 'party', 'street1', 'street2', 'city', 'state', 'zip', 'phone', 'home', 'cell', 'email', 'committees', 'committees2', 'committees3'])
+  houseTable = DictReader(StringIO(houseText), delimiter = '*', fieldnames = ['chamber', 'fullName', 'lastName', 'firstName', 'middleName', 'town', 'district', 'id', 'party', 'street1', 'street2', 'city', 'state', 'zip', 'phone', 'home', 'cell', 'email', 'committees', 'committees2', 'committees3'])
   senateTable = senateSoup.find('table', {'id': 'nhsenators'}).find_all('tr')
 
   dictList = []
@@ -15,7 +15,7 @@ def getNHLeg(partyDict):
   for item in houseTable:
     repInfo = {}
 
-    repInfo['District'] = 'NH State {0} District {1} {2}'.format(item['chamber'], item['town'], item['district'])
+    repInfo['District'] = 'NH State {0} District {1} {2}'.format(item['chamber'], item['town'], int(item['district']))
     repInfo['Party'] = partyDict[str(item['party'])]
     repInfo['Name'] = '{0} {1} {2}'.format(item['firstName'], item['middleName'], item['lastName']).strip().replace('    ', ' ').replace('  ', ' ').replace(u'\u00A0', ' ').replace('   ', ' ').replace('  ', ' ').replace(u'\u0144','n').replace(u'\u00f1','n').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u201A',"'").replace(u'\u201B',"'").replace(u'\u2039',"'").replace(u'\u203A',"'").replace(u'\u201C','"').replace(u'\u201D','"').replace(u'\u201E','"').replace(u'\u201F','"').replace(u'\u00AB','"').replace(u'\u00BB','"').replace(u'\u00e0','a').replace(u'\u00e1','a').replace(u'\u00e8','e').replace(u'\u00e9','e').replace(u'\u00ec','i').replace(u'\u00ed','i').replace(u'\u00f2','o').replace(u'\u00f3','o').replace(u'\u00f9','u').replace(u'\u00fa','u')
     repInfo['Phone'] = item['phone']
